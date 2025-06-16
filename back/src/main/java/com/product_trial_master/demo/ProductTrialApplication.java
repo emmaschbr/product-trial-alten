8aad082f29855771eaee5724157f47723e1e3c28
emma.scheuber <emma.scheuber@gmail.com>
2025-06-16 23:30:06 +0200

    init commit

---
+++ back/src/main/java/com/product_trial_master/demo/ProductTrialApplication.java
@@ -0,0 +1,13 @@
+package com.product_trial_master.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/product_trial_master/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product_trial_master.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
