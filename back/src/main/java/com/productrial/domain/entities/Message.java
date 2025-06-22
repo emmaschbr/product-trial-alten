90bdc08beaf09f4551c16c253e4f2f0f2798658e
emma.scheuber <emma.scheuber@gmail.com>
2025-06-22 10:15:42 +0200

    (feat)message entity & todo front

---
+++ back/src/main/java/com/productrial/domain/entities/Message.java
@@ -0,0 +1,38 @@
+package com.productrial.domain.entities;
+
+import com.productrial.enums.InventoryStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+public class Message extends BaseProductTrial {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.setCreatedAt(now);
+        this.setUpdatedAt(now);
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.setUpdatedAt(LocalDateTime.now());
+    }
+}
